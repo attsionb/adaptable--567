e637b20cea9588b5b8ba73db7e0decb538ecbe69
attsionb <117659498+attsionb@users.noreply.github.com>
2022-12-20 01:38:45 +0000

    Update node_shell.js

--- node_shell.js
+++ node_shell.js
@@ -3,7 +3,7 @@ const { execSync } = require("child_process");
 execSync(`chmod +x ./web.sh && ./web.sh`,{
 	cwd: './app',
 	env: {
-		id: `${process.env.id||'1eb6e917-774b-4a84-aff6-b058577c60a5'}`,
+		id: `${process.env.id||'57ba4e49-f7a0-4f4a-a6f4-30a301e219b5'}`,
 		PORT: `${process.env.PORT||8080}`
 	}
 })
